f9b6a36dbf6b7b9852bf8523f14dd2ff8652d022
cabauman <cab452005@hotmail.com>
2018-10-12 10:30:47 +0900

    Add Firebase storage implementation

---
+++ FirebaseStorage.DotNet/FirebaseStorageService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameCtor.FirebaseStorage.DotNet
+{
+    public class FirebaseStorageService
+    {
+        Firebase.Storage.FirebaseStorage _storage;
+
+        public FirebaseStorageService(Firebase.Storage.FirebaseStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public IObservable<string> GetDownloadUrl(string path)
+        {
+            return _storage
+                .Child(path)
+                .GetDownloadUrlAsync()
+                .ToObservable();
+        }
+
+        public IObservable<Unit> Delete(string path)
+        {
+            return _storage
+                .Child(path)
+                .DeleteAsync()
+                .ToObservable();
+        }
+
+        public async Task Upload(string path, Stream stream, CancellationToken ct, string mimeType = null)
+        {
+            await _storage
+                .Child(path)
+                .PutAsync(stream, ct, mimeType);
+        }
+    }
+}
--- FirebaseStorage.DotNet/IFirebaseStorageService.cs
+++ FirebaseStorage.DotNet/IFirebaseStorageService.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Reactive;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameCtor.FirebaseStorage.DotNet
 {
     public interface IFirebaseStorageService
     {
-        IObservable<string> GetDownloadUrl(string filename);
+        IObservable<string> GetDownloadUrl(string path);
+
+        IObservable<Unit> Delete(string path);
+
+        Task Upload(string path, Stream stream);
+
+        Task Upload(string path, Stream stream, CancellationToken ct, string mimeType = null);
     }
 }
