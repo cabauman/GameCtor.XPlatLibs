f32c1d165c84e6567e8d7112d8f3062fccc4e8ff
cabauman <cab452005@hotmail.com>
2019-02-22 22:22:59 +0900

    Experiment with a cache repo (wip)

---
+++ FirebaseDatabase.DotNet/FirebaseOfflineCacheRepo.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Offline;
+using Firebase.Database.Query;
+using GameCtor.Repository;
+
+namespace GameCtor.FirebaseDatabase.DotNet
+{
+    public class FirebaseOfflineCacheRepo<T> : IFirebaseRepository<T>
+        where T : class, IModel
+    {
+        private readonly RealtimeDatabase<T> _realtimeDb;
+        private readonly ChildQuery _baseQuery;
+
+        public FirebaseOfflineCacheRepo(
+            FirebaseClient client,
+            string path,
+            string key = "",
+            StreamingOptions streaming = StreamingOptions.None,
+            InitialPullStrategy initialPull = InitialPullStrategy.Everything)
+        {
+            // The offline database filename is named after type T.
+            // So, if you have more than one list of type T objects, you need to differentiate it
+            // by adding a filename modifier; which is what we're using the "key" parameter for.
+            _baseQuery = client.Child(path);
+            _realtimeDb = _baseQuery
+                .AsRealtimeDatabase<T>(key, string.Empty, streaming, initialPull, true);
+
+            SyncExceptionThrown = Observable
+                .FromEventPattern<ExceptionEventArgs>(
+                    h => _realtimeDb.SyncExceptionThrown += h,
+                    h => _realtimeDb.SyncExceptionThrown -= h);
+        }
+
+        public IObservable<EventPattern<ExceptionEventArgs>> SyncExceptionThrown { get; }
+
+        public IObservable<Unit> Add(T item)
+        {
+            return Observable
+                .Start(() => _realtimeDb.Post(item))
+                .Do(itemKey => item.Id = itemKey)
+                .Select(_ => Unit.Default);
+        }
+
+        public IObservable<Unit> Add(IEnumerable<T> items)
+        {
+            // Doesn't work offline. Need offline solution.
+            return _baseQuery
+                .PatchAsync(items.ToDictionary(x => x.Id = FirebaseKeyGenerator.Next()))
+                .ToObservable()
+                .SelectMany(_realtimeDb.PullAsync().ToObservable());
+        }
+
+        public IObservable<Unit> Upsert(T item)
+        {
+            return Observable
+                .Start(() => _realtimeDb.Patch(item.Id, item));
+        }
+
+        public IObservable<Unit> Upsert(IEnumerable<T> items)
+        {
+            // Doesn't work offline. Need offline solution.
+            return _baseQuery
+                .PatchAsync(items.ToDictionary(x => x.Id = x.Id ?? FirebaseKeyGenerator.Next()))
+                .ToObservable()
+                .SelectMany(_ => _realtimeDb.PullAsync().ToObservable());
+        }
+
+        public IObservable<Unit> Delete(string id)
+        {
+            return Observable
+                .Start(() => _realtimeDb.Delete(id));
+        }
+
+        public IObservable<Unit> Delete(IEnumerable<T> items)
+        {
+            // Doesn't work offline. Need offline solution.
+            return _baseQuery
+                .PatchAsync(items.ToDictionary(x => x.Id, x => default(string)))
+                .ToObservable()
+                .Concat(_realtimeDb.PullAsync().ToObservable());
+        }
+
+        public IObservable<T> GetItem(string id)
+        {
+            return Observable
+                .Start(
+                    () =>
+                    {
+                        _realtimeDb.Database.TryGetValue(id, out OfflineEntry item);
+                        return item?.Deserialize<T>();
+                    });
+        }
+
+        public IObservable<IEnumerable<T>> GetItems(bool fetchOnline = false)
+        {
+            return Observable
+                .Return(fetchOnline || _realtimeDb.Database.Count == 0 ? Pull() : Observable.Return(Unit.Default))
+                .SelectMany(x => x)
+                .SelectMany(_ => _realtimeDb.Once())
+                .Do(MapKeyToId)
+                .Select(x => x.Object)
+                .ToList();
+        }
+
+        private static readonly Dictionary<Firebase.Database.Streaming.FirebaseEventSource, FirebaseEventSource> EventSourceMap = new Dictionary<Firebase.Database.Streaming.FirebaseEventSource, FirebaseEventSource>
+        {
+            { Firebase.Database.Streaming.FirebaseEventSource.Online, FirebaseEventSource.Online },
+            { Firebase.Database.Streaming.FirebaseEventSource.OnlineInitial, FirebaseEventSource.Online },
+            { Firebase.Database.Streaming.FirebaseEventSource.OnlinePull, FirebaseEventSource.Online },
+            { Firebase.Database.Streaming.FirebaseEventSource.OnlinePush, FirebaseEventSource.Online },
+            { Firebase.Database.Streaming.FirebaseEventSource.OnlineStream, FirebaseEventSource.Online },
+            { Firebase.Database.Streaming.FirebaseEventSource.Offline, FirebaseEventSource.Offline },
+        };
+
+        private static readonly Dictionary<Firebase.Database.Streaming.FirebaseEventType, FirebaseEventType> EventTypeMap = new Dictionary<Firebase.Database.Streaming.FirebaseEventType, FirebaseEventType>
+        {
+            { Firebase.Database.Streaming.FirebaseEventType.InsertOrUpdate, FirebaseEventType.AddOrUpdate },
+            { Firebase.Database.Streaming.FirebaseEventType.Delete, FirebaseEventType.Delete },
+        };
+
+        public IObservable<FirebaseEvent<T>> Observe()
+        {
+            return _realtimeDb
+                .AsObservable()
+                .Do(MapKeyToId)
+                .Select(
+                    x =>
+                    {
+                        EventSourceMap.TryGetValue(x.EventSource, out var eventSource);
+                        EventTypeMap.TryGetValue(x.EventType, out var eventType);
+                        return new FirebaseEvent<T>(x.Key, x.Object, eventSource, eventType);
+                    });
+        }
+
+        private IObservable<Unit> Pull()
+        {
+            return _realtimeDb
+                .PullAsync()
+                .ToObservable();
+        }
+
+        private void MapKeyToId(FirebaseObject<T> firebaseObj)
+        {
+            firebaseObj.Object.Id = firebaseObj.Key;
+        }
+    }
+}
