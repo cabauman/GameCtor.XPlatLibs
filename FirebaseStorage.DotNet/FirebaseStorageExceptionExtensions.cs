da8b914c2c83d95caee6a06765e0964ffd8da3de
cabauman <cab452005@hotmail.com>
2019-02-22 22:15:01 +0900

    Add storage exception extensions

---
+++ FirebaseStorage.DotNet/FirebaseStorageExceptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Firebase.Storage;
+using Newtonsoft.Json.Linq;
+
+namespace GameCtor.FirebaseStorage.DotNet
+{
+    public static class FirebaseStorageExceptionExtensions
+    {
+        public static string GetErrorCode(this FirebaseStorageException @this)
+        {
+            var o = JObject.Parse(@this.ResponseData);
+            return o["error"]["code"].ToString();
+        }
+
+        public static bool ObjectNotFound(this FirebaseStorageException @this)
+        {
+            return @this.GetErrorCode() == "404";
+        }
+    }
+}
